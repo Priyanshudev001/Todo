5fa1c8716ebfc1e4b0250f618afcbeb3fd92131f
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-06-22 14:57:10 +0545

    form key validadtion

--- lib/addtodo.dart
+++ lib/addtodo.dart
@@ -1,8 +1,18 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-class Addtodo extends StatelessWidget {
+class Addtodo extends StatefulWidget {
   const Addtodo({super.key});
 
+  @override
+  State<Addtodo> createState() => _AddtodoState();
+}
+
+class _AddtodoState extends State<Addtodo> {
+  final formkey = GlobalKey<FormState>();
+  final title = TextEditingController();
+  final description = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +33,45 @@ class Addtodo extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: formkey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: title,
+                decoration: InputDecoration(label: Text('Title')),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Title is required";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: description,
+                maxLines: 3,
+                decoration: InputDecoration(label: Text("description")),
+              ),
+
+              SizedBox(height: 12),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formkey.currentState?.validate() == true) {
+                      log('j');
+                    }
+                  },
+                  child: Text("Submit"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
